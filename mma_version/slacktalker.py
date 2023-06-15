eee79ca110cdbab90eeba82e19ee94c77d7d6571
Michael Murphy <mdmurphyy@gmail.com>
2023-06-15 13:57:53 -0400

    auto create new channels

--- mma_version/slacktalker.py
+++ mma_version/slacktalker.py
@@ -16,7 +16,7 @@ class slack_bot:
 
     def __init__(self):
         self._SLACK_TOKEN = SLACK_TOKEN
-        self.default_channel = 'gw-frb-listener'
+        #self.default_channel = 'gw-frb-listener'
 
         self.client = WebClient(token=SLACK_TOKEN)
 
@@ -34,28 +34,28 @@ class slack_bot:
             else:
                 print("\nCould not create new channel. Error: ", e.response["error"])
 
-    def post_short_message( self, channel_name, message_text, verbose:bool=False ):
+    def post_short_message( self, channel_name, message_text, verbose:bool=False, _counter:int=0 ):
         # This is a message without buttons and stuff. We are assuming #alert-bot-test already exists and the bot is added to it
         # If it fails, create #alert-bot-test or similar channel and BE SURE to add the slack bot app to that channel or it cannot send a message to it!
         try:
             print("Trying to send message to ns channel...", end='')
             response = self.client.chat_postMessage(channel=channel_name, text=message_text)
+            if verbose: print(response)
             print("Done")
         except SlackApiError as e:
-            print("\nCould not post message. Error: ", e.response["error"])
+            if e.response["error"] == 'channel_not_found' and _counter==0:
+                self.create_new_channel(channel_name)
+                self.post_short_message( channel_name, message_text, verbose, _counter=1 )
+            else: 
+                print("\nCould not post message. Error: ", e.response["error"])
                             
 
 
-    def post_message( self, title:str, message_text,  channel_name:str=None, verbose:bool=False, _counter:int=0):
-        if channel_name is None: channel_name = self.default_channel
+    def post_message( self, title:str, message_text,  channel_name:str, verbose:bool=False, _counter:int=0):
+        #if channel_name is None: channel_name = self.default_channel
         # This is a message with buttons and stuff. 
         # TODO: add buttons and stuff
         try:
-            '''
-            print("Trying to send message to general channel...", end='')
-            response = self.client.chat_postMessage(channel=channel_name, text=message_text)
-            if verbose: print(response)
-            print("Done")'''
             print("Trying to send message to event channel...",end='')
             response = self.client.chat_postMessage(
                                     channel=channel_name,
